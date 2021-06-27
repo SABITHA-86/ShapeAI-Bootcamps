b3289fac29ed8e66bf714e089805dad939a17e07
SABITHA-86 <85760323+SABITHA-86@users.noreply.github.com>
2021-06-27 14:43:36 +0530

    Add files via upload

---
+++ Bootcamps/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info1 from "./Info1";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info1 />
+      <Info1 />
+      <Info1 />
+      <Info1 />
+      <Info1 />
+      <Info1 />
+    </div>
+  );
+}
+export default App;
---
+++ Bootcamps/src/components/Info1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info1() {
+  return (
+    <div className="note">
+      <h1>Javascript,React Js and Node Js</h1>
+      <p>A basic knowledge of html,css,js,react.js and node.js</p>
+    </div>
+  );
+}
+export default Info1;
